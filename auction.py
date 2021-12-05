ba0dc005966ef9fe874a9d132085d5dcee2ad3a0
Eric <ericblarson125@gmail.com>
2021-12-04 22:22:18 -0800

    removed unknown bidders

--- auction.py
+++ auction.py
@@ -13,6 +13,8 @@ def getBidAdjustment(bidName):
 	for bidder in config["bidders"]:
 		if bidder["name"] == bidName:
 			return bidder["adjustment"]
+		else:
+			return None
 
 
 def getSiteIndex(siteName):
@@ -56,53 +58,23 @@ for index, auction in enumerate(inputData):
 		
 		for bid in auction["bids"]:
 			# check if bid: unit is in auction: units list
-			winningBids
-
-			if bid["unit"] in winningBids.keys():
+			
+			# when do I check the bidder?
+			if bid["unit"] in maxBids.keys():
 				bidAdjust = getBidAdjustment(bid["bidder"])
-				adjustedBid = (bid["bid"] * bidAdjust) + bid["bid"]
+				if bidAdjust is not None:
+					adjustedBid = (bid["bid"] * bidAdjust) + bid["bid"]
 
-				if adjustedBid > config["sites"][siteIndex]["floor"]:
-					# always get the value of the unit index from dictioanry
-					if (maxBids[bid["unit"]] is None):
-						maxBids[bid["unit"]] = adjustedBid
-						winningBids.update({bid["unit"]: bid})
-					if adjustedBid > maxBids[bid["unit"]]:
-						maxBids[bid["unit"]] = adjustedBid
-						winningBids[bid["unit"]] = bid
-			#print(winningBids)
-			#print(maxBids)			
+					if adjustedBid > config["sites"][siteIndex]["floor"]:
+						# always get the value of the unit index from dictioanry
+						if maxBids[bid["unit"]] is None:
+							maxBids[bid["unit"]] = adjustedBid
+							winningBids.update({bid["unit"]: bid})
+						if adjustedBid > maxBids[bid["unit"]]:
+							maxBids[bid["unit"]] = adjustedBid
+							winningBids[bid["unit"]] = bid
+				print(winningBids)
+				print(maxBids)			
 	print(" ")
-					#if adjustedBid > highestBid[unitDict[bid["unit"]]]
 
 
-# how store the bid and the adjustment?
-# push to highestBids at the end of for loop
-# use a maxBid value to store the adjustments
-# use a dictionary to store the 
-
-""""
-
-			if bid["unit"] in auction["units"]:
-				bidIndex = auction["units"].index(bid["unit"])
-				# create a key: value???
-				# key is the unit and value is the index for highestbid?
-
-				bidAdjust = getBidAdjustment(bid["bidder"])
-				adjustedBid = bid["bid"] * bidAdjust + bid["bid"]
-
-				if adjustedBid > config["sites"][siteIndex]["floor"]:
-					# highestBids[index] is site level  [ ]
-					# highestBids[index][bidIndex] is bid level [[], ]  so highestBids[0] returns []
-					if not highestBids[index]:
-						# site bid list is empty
-						highestBids[index].append(bid)
-
-					# how to match the units and the prices?
-					#if bid["unit"]
-					#if adjustedBid > highestBids[index][bidIndex]["bid"]:
-					#highestBids[index][bidIndex] = bid
-
-				#print(highestBids)
-
-"""
