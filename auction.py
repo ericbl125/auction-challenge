9520ce204e178c2e80b03b83fcfc6be52301c8c4
Eric <ericblarson125@gmail.com>
2021-12-05 20:36:24 -0800

    finished

--- auction.py
+++ auction.py
@@ -1,26 +1,29 @@
-# python3 vesion 3.8.10
+#!/usr/bin/python
 
 # Auction challenge for Freestar
 import json
+import sys
+import os
 
 
-def getBidAdjustment(bidName):
+def getBidAdjustment(bidName, config):
 	"""
 	Description: Searches the config file for the bidder with a matching name returns the adjustment
 	:param siteName: string value of the name of the bidder
+	:param config: dictionary containing the configuration values
 	:return: adjustment value of bidder
 	"""	
 	for bidder in config["bidders"]:
 		if bidder["name"] == bidName:
 			return bidder["adjustment"]
-		else:
-			return None
+	return None
 
 
-def getSiteIndex(siteName):
+def getSiteIndex(siteName, config):
 	"""
 	Description: Searches the config file for the site with a matching name returns the index
 	:param siteName: string value of the name of the site
+	:param config: dictionary containing the configuration values
 	:return: index value of the site in the config file
 	"""
 	for index, configSite in enumerate(config["sites"]):
@@ -30,51 +33,49 @@ def getSiteIndex(siteName):
 	return -1
 	
 
-with open('config.json') as file:
-	config = json.load(file)
+def main(argv):
+
+	with open("config.json") as file:
+		config = json.load(file)
 
 
-with open('input.json') as file:
-	inputData = json.load(file)
+	with open(argv[0]) as file:
+		inputData = json.load(file)
 
-# create key:value for units for every site
-# {sidebar:0, banner:1}
+	# create key:value for units for every site
+	highestBids = []
+	for auction in inputData:
 
-highestBids = []
-for index, auction in enumerate(inputData):
-	# create unit dictionary for faster ordering of winning bids
-	# unitDict = {unit: idx for idx, unit in enumerate(auction["units"])}
+		# store and validate the site
+		siteIndex = getSiteIndex(auction["site"], config)
 
-	maxBids = dict.fromkeys(auction["units"])
-	winningBids = {}
-	
-	# cache the config site info
-	siteIndex = getSiteIndex(auction["site"])
-
-	# validate the site
-	if siteIndex > -1:
-		# add list entry to highestBids for auctions
-		# highestBids.append([None]*len(unitDict))
-		
-		for bid in auction["bids"]:
-			# check if bid: unit is in auction: units list
+		if siteIndex > -1:
+			# dictionaries to store maxBids and the current winning bidder information
+			maxBids = dict.fromkeys(auction["units"])
+			winningBids = {}
 			
-			# when do I check the bidder?
-			if bid["unit"] in maxBids.keys():
-				bidAdjust = getBidAdjustment(bid["bidder"])
-				if bidAdjust is not None:
-					adjustedBid = (bid["bid"] * bidAdjust) + bid["bid"]
+			for bid in auction["bids"]:
+				# validate the bidder in the to config list of approved bidders
+				if bid["bidder"] in config["sites"][siteIndex]["bidders"]:
+					if bid["unit"] in maxBids.keys():
 
-					if adjustedBid > config["sites"][siteIndex]["floor"]:
-						# always get the value of the unit index from dictioanry
-						if maxBids[bid["unit"]] is None:
-							maxBids[bid["unit"]] = adjustedBid
-							winningBids.update({bid["unit"]: bid})
-						if adjustedBid > maxBids[bid["unit"]]:
-							maxBids[bid["unit"]] = adjustedBid
-							winningBids[bid["unit"]] = bid
-				print(winningBids)
-				print(maxBids)			
-	print(" ")
+						bidAdjust = getBidAdjustment(bid["bidder"], config)
+						if bidAdjust is not None:
+							adjustedBid = (bid["bid"] * bidAdjust) + bid["bid"]
 
+							if adjustedBid > config["sites"][siteIndex]["floor"]:
+								# always get the value of the unit index from dictioanry
+								if maxBids[bid["unit"]] is None:
+									maxBids[bid["unit"]] = adjustedBid
+									winningBids.update({bid["unit"]: bid})
+								if adjustedBid > maxBids[bid["unit"]]:
+									maxBids[bid["unit"]] = adjustedBid
+			highestBids.append(list(winningBids.values()))
+	# make it JSON
+	output = json.dumps(highestBids)
+	print(output)
 
+if __name__ == '__main__':
+	# How to read the input files.
+	print(sys.argv)
+	main(sys.argv)
